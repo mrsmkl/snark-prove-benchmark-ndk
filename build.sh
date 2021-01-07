a2f1559f9919fe43e8016c183a170bd3487509d0
Sami Mäkelä <mrsmkl@gmail.com>
2021-01-07 17:56:20 +0200

    working

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
 cross build --release --target=armv7-linux-androideabi
 cross build --release --target=aarch64-linux-android
 cross build --release --target=i686-linux-android
+
+cp target/aarch64-linux-android/release/libsnark_prove_benchmark_ndk.so ~/AndroidStudioProjects/SnarkBenchmark2/app/src/main/jniLibs/arm64-v8a
+cp target/armv7-linux-androideabi/release/libsnark_prove_benchmark_ndk.so ~/AndroidStudioProjects/SnarkBenchmark2/app/src/main/jniLibs/armeabi
+cp target/i686-linux-android/release/libsnark_prove_benchmark_ndk.so ~/AndroidStudioProjects/SnarkBenchmark2/app/src/main/jniLibs/x86
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::os::raw::{c_char};
-use std::ffi::{CString, CStr};
+use std::ffi::{CString};
 use std::format;
 use epoch_snark::{prove, trusted_setup, verify};
 use std::time::Instant;
@@ -204,7 +204,7 @@ fn generate_bitmaps(num_epochs: usize, num_validators: usize, faults: usize) -> 
 }
 
 #[no_mangle]
-pub extern fn rust_greeting(_to: *const c_char) -> *mut c_char {
+pub extern fn run_bench() -> *mut c_char {
     let rng = &mut rand::thread_rng();
     let num_validators = 1;
     let num_epochs = 1;
@@ -251,15 +251,12 @@ pub mod android {
 
     use super::*;
     use self::jni::JNIEnv;
-    use self::jni::objects::{JClass, JString};
+    use self::jni::objects::JClass;
     use self::jni::sys::{jstring};
 
     #[no_mangle]
-    pub unsafe extern fn Java_com_example_rusttest_RustTest_greeting(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
-        // Our Java companion code might pass-in "world" as a string, hence the name.
-        let world = rust_greeting(env.get_string(java_pattern).expect("invalid pattern string").as_ptr());
-        // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
-        let world_ptr = CString::from_raw(world);
+    pub unsafe extern fn Java_org_celo_snarkbenchmark_Bench_bench(env: JNIEnv, _: JClass) -> jstring {
+        let world_ptr = CString::from_raw(run_bench());
         let output = env.new_string(world_ptr.to_str().unwrap()).expect("Couldn't create java string!");
 
         output.into_inner()
